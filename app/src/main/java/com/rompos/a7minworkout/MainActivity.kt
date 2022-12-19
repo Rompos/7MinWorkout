1647513c68184ecc0701931e5c7ba01260339ac2
Stratos Rompos <stratosrom@hotmail.com>
2022-12-19 18:50:51 +0200

    Animation buttons

--- app/src/main/java/com/rompos/a7minworkout/MainActivity.kt
+++ app/src/main/java/com/rompos/a7minworkout/MainActivity.kt
@@ -3,9 +3,8 @@ package com.rompos.a7minworkout
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.FrameLayout
-import android.widget.Toast
 import com.rompos.a7minworkout.databinding.ActivityMainBinding
+import android.graphics.drawable.AnimatedVectorDrawable
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,30 +22,36 @@ class MainActivity : AppCompatActivity() {
 //        val flStartButton : FrameLayout = findViewById(R.id.flStart)
 
         binding?.flStart?.setOnClickListener{
-        //flStartButton.setOnClickListener{  //redundant with binding
+            //flStartButton.setOnClickListener{  //redundant with binding
             //Toast.makeText(this@MainActivity,"Here we will start the exercise.",Toast.LENGTH_SHORT).show()
-
             val intent = Intent(this,ExerciseActivity::class.java)
             startActivity(intent)
-
         }
 
         binding?.flBMI?.setOnClickListener{
             //flStartButton.setOnClickListener{  //redundant with binding
             //Toast.makeText(this@MainActivity,"Here we will start the exercise.",Toast.LENGTH_SHORT).show()
-
             val intent = Intent(this,BMIActivity::class.java)
             startActivity(intent)
-
         }
 
         binding?.flHistory?.setOnClickListener{
             //flStartButton.setOnClickListener{  //redundant with binding
             //Toast.makeText(this@MainActivity,"Here we will start the exercise.",Toast.LENGTH_SHORT).show()
-
             val intent = Intent(this,HistoryActivity::class.java)
             startActivity(intent)
+        }
 
+        var isChecked = false
+        binding?.avdImage!!.setOnClickListener {
+            if (isChecked){
+                checkToClose()
+                this.finish()
+            }
+            else{
+                closeToCheck()
+            }
+            isChecked = !isChecked
         }
     }
 
@@ -56,4 +61,17 @@ class MainActivity : AppCompatActivity() {
         binding = null
     }
 
+    private fun closeToCheck() {
+
+        binding?.avdImage!!.setImageResource(R.drawable.avd_close_to_check)
+        val avdCheckToClose = binding?.avdImage!!.drawable as AnimatedVectorDrawable
+        avdCheckToClose.start()
+    }
+
+    private fun checkToClose() {
+
+        binding?.avdImage!!.setImageResource(R.drawable.avd_check_to_close)
+        val avdCheckToClose = binding?.avdImage!!.drawable as AnimatedVectorDrawable
+        avdCheckToClose.start()
+    }
 }
